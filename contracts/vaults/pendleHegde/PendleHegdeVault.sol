e8cbccbad4836b46fdaed1880d550e0d81849a5a
vuxbasnin <banincora@gmail.com>
2024-08-07 21:31:34 +0700

    [Rock Onyx] add deposit pendle vault

--- contracts/vaults/pendleHegde/PendleHegdeVault.sol
+++ contracts/vaults/pendleHegde/PendleHegdeVault.sol
@@ -2,36 +2,54 @@
 pragma solidity ^0.8.19;
 
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
-import "./structs/PendleStruct.sol";
-import "../../extensions/RockOnyxAccessControl.sol";
+import "../restakingDeltaNeutral/Base/strategies/PerpDexStrategy.sol";
+import "../../extensions/TransferHelper.sol";
+import "../pendleHegde/structs/PendleStruct.sol";
 
-contract PendleHegdeVault is Initializable, RockOnyxAccessControl {
-    VaultParams internal vaultParams;
-    VaultState internal vaultState;
+contract PendleHegdeVault is Initializable, PerpDexStrategy {
+    VaultParamsPendle internal vaultParams;
+    VaultStatePendle internal vaultState;
     uint256 internal initialPPS;
+    mapping(address => DepositReceiptPendle) internal depositReceipts;
 
     function initialize(
         address _admin,
-        address _ptToken,
         address _usdc,
         uint8 _decimals,
         uint256 _minimunSupply,
         uint256 _cap,
+        address _perpDexAddress,
+        address _perpDexReceiver,
+        address _perpDexConnector,
         uint256 _initialPPS
     ) public initializer {
-        vaultParams = VaultParams(
+        vaultParams = VaultParamsPendle(
             _decimals,
             _usdc,
-            _ptToken,
             _minimunSupply,
             _cap,
             10,
             1
         );
-        vaultState = VaultState(0, 0, 0, 0, block.timestamp);
+        vaultState = VaultStatePendle(0, 0, 0, 0, block.timestamp);
         initialPPS = _initialPPS;
+        paused = false;
+
+        perpDex_Initialize(_perpDexAddress, _perpDexReceiver, _usdc, _perpDexConnector);
 
         _grantRole(ROCK_ONYX_ADMIN_ROLE, _admin);
         _grantRole(ROCK_ONYX_OPTIONS_TRADER_ROLE, _admin);
+    }
+
+    /**
+     * @param _ptTokenAmount amount ptToken deposit == amount usdc
+     */
+    function depositToPendleHegdeVault(uint256 _ptTokenAmount, address _ptToken) external nonReentrant {
+        require(paused == false, "VAULT_PAUSED");
+        require(_ptTokenAmount > 0, "INVALID_AMOUNT_DEPOSIT");
+        TransferHelper.safeTransferFrom(vaultParams.asset, msg.sender, address(this), _ptTokenAmount);
+        depositReceipts[msg.sender].depositAmount += _ptTokenAmount;
+        depositReceipts[msg.sender].ptToken = _ptToken;
+        this.depositToVendor(30000);
     }
 }
--- contracts/vaults/pendleHegde/structs/PendleStruct.sol
+++ contracts/vaults/pendleHegde/structs/PendleStruct.sol
@@ -1,20 +1,25 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.19;
 
-struct VaultParams {
+struct VaultParamsPendle {
     uint8 decimals;
     address asset;
-    address ptToken;
     uint256 minimumSupply;
     uint256 cap;
     uint256 performanceFeeRate;
     uint256 managementFeeRate;
 }
 
-struct VaultState {
+struct VaultStatePendle {
     uint256 withdrawPoolAmount;
     uint256 pendingDepositAmount;
     uint256 totalShares;
     uint256 totalFeePoolAmount;
     uint256 lastUpdateManagementFeeDate;
+}
+
+struct DepositReceiptPendle {
+    uint256 shares;
+    uint256 depositAmount;
+    address ptToken;
 }
